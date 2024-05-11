157e4b891106755b27a6780ab16b78a207c8984a
BFKazmierczak <bfkazmierczak@gmail.com>
2024-05-11 11:26:24 +0200

    install react

--- scripts/starManipulation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('skyCanvas')
-
-  console.log('twoja stara')
-
-  canvas.addEventListener('click', (event) => {
-    console.log({ event })
-  })
-})
